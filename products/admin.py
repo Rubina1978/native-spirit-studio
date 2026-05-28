16326b84f06ad6c4e5d7f648efabe98320df0911
Rubina1978 <hrubesova@gmail.com>
2026-05-28 10:39:54 +0100

    Add functionality for filtering products, queries and categories

--- products/admin.py
+++ products/admin.py
@@ -26,5 +26,4 @@ class CategoryAdmin(admin.ModelAdmin,):
 
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Category, CategoryAdmin)#
-
+admin.site.register(Category, CategoryAdmin)
