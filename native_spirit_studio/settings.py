526858bfabec22f482e0984cf83041d8320c27c9
Rubina1978 <hrubesova@gmail.com>
2026-05-30 17:15:54 +0100

    Added shopping bag app, urls and template

--- native_spirit_studio/settings.py
+++ native_spirit_studio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s$c)n(scznr3ml%g@gu_zr=av(yr)dmea9bwd7t8q3h*ae2q3v'
+SECRET_KEY = 'django-insecure-s$c)n(scznr3ml%g@gu_zr=av(yr)dmea9bwd7t8q3h*ae2q3v'   # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
