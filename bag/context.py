4e1651d23986f6c0720fc6ab6f9fd3b4ec42fbd3
Rubina1978 <hrubesova@gmail.com>
2026-05-31 23:35:40 +0100

    made shopping bag table to display shopping, price, number of items and delivery

--- bag/context.py
+++ bag/context.py
@@ -19,6 +19,7 @@ def bag_contents(request):
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
+            'total': quantity * product.price,
 
         })
 
