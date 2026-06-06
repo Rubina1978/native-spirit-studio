cbe57f1eca10a1769157e04b815e6a9950bf1fd9
Rubina1978 <hrubesova@gmail.com>
2026-06-06 21:04:40 +0100

    Add stripe

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51TfOSzHMPnHHIyxwbTZsWrJucQ0olArIyKhGaZb8LMRQdqJQ5cZFyI6AV5HIGYtEX1z0FThD0l7oXqOmHa6mZL8u00Rc8Ec2s6',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
