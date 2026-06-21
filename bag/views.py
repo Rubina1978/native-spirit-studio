03be1b4ebd1ed74036e21af7c4368773c4f8c684
Rubina1978 <hrubesova@gmail.com>
2026-06-21 19:56:02 +0100

    checked for errors with flake8 and resolved

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404, reverse, HttpResponse
+from django.shortcuts import (
+    render, redirect, get_object_or_404,
+    reverse, HttpResponse
+    )
+
 from django.contrib import messages
 from products.models import ProductSize, Product
 
--- native_spirit_studio/settings.py
+++ native_spirit_studio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
-    import env
+    import env          # noqa: F401
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
