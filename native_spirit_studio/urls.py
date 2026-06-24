88f7526b0b4135227c0984511e8dafe538d80865
Rubina1978 <hrubesova@gmail.com>
2026-06-24 22:29:28 +0100

    add TESTING.md

--- native_spirit_studio/urls.py
+++ native_spirit_studio/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +28,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('journal/', include('journal.urls')),
-    path('test-404/', TemplateView.as_view(template_name='errors/404.html'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
