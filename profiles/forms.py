16ed5f25fa026bf85bced05293b0d7443c0802e4
Rubina1978 <hrubesova@gmail.com>
2026-06-25 01:21:37 +0100

    removed placeholder from coutry select as w3c complains

--- profiles/forms.py
+++ profiles/forms.py
@@ -29,6 +29,9 @@ class UserProfileForm(forms.ModelForm):
                     placeholder = f'{placeholders[field]} *'
                 else:
                     placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'    # noqa
-            self.fields[field].label = False
+
+        self.fields[field].widget.attrs['placeholder'] = placeholder
+
+        self.fields[field].widget.attrs[
+            'class'] = 'border-black rounded-0 profile-form-input'
+        self.fields[field].label = False
