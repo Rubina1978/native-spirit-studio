71e6ff1af3a05a90909ca62dc67705f0c3b03d40
Rubina1978 <hrubesova@gmail.com>
2026-06-26 17:37:26 +0100

    fix linter errors

--- checkout/models.py
+++ checkout/models.py
@@ -95,7 +95,7 @@ class OrderLineItem(models.Model):
         """ Override the original save method to set the lineitem total
         and update the order total
         """
-        self.lineitem_total = self.size_price * self.quantity if self.size_price else self.product.price * self.quantity
+        self.lineitem_total = self.size_price * self.quantity if self.size_price else self.product.price * self.quantity   # noqa
         super().save(*args, **kwargs)
 
     def __str__(self):
